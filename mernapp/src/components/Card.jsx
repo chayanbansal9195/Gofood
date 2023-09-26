ebd7c4642e644fdba1fdbc55de7a23fe73c36d2b
Chayan Bansal <chayanbansal1999@gmail.com>
2023-09-27 02:38:15 +0530

    gofood mern app added

---
+++ mernapp/src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <div>
+        <div
+          className="card mt-3"
+          style={{ width: "18rem", maxHeight: "360px" }}
+        >
+          <img src="https://media.istockphoto.com/id/1311491582/photo/chilli-paneer-indian-snack-food.jpg?s=170667a&w=0&k=20&c=CxYpu-8c-SEJ1ej9-kVnuCEk7VbunaLlutYm_lE821o=" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is some imprtant text</p>
+            <div className="container w-100">
+              <select className="m-2 h-100 bg-success rounded">
+                {Array.from(Array(6), (e, i) => {
+                  return (
+                    <option key={i + 1} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  );
+                })}
+              </select>
+              <select className="m-2 h-100 bg-success rounded">
+                <option value="half">Half</option>
+                <option value="full">Full</option>
+              </select>
+              <div className="d-inline h-100 fs-5">Total Price</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
