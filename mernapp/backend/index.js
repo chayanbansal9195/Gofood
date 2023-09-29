af978f64c9eb3da57dfcbe546f2b4e75c4b6fc12
Chayan Bansal <chayanbansal1999@gmail.com>
2023-09-29 22:11:26 +0530

    added signup

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -6,12 +6,21 @@ const port = 5000;
 
 mongoDb();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next()
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use(express.json())
-app.use("/api/", require("./routes/CreateUser"))
+app.use(express.json());
+app.use("/api/", require("./routes/CreateUser"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
