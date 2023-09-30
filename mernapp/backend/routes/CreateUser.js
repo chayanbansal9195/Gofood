2640a75fc68ff278e4ea247a750bd5c628966600
Chayan Bansal <chayanbansal1999@gmail.com>
2023-09-30 14:27:31 +0530

    added login code with bcrypt jwt

--- mernapp/backend/routes/CreateUser.js
+++ mernapp/backend/routes/CreateUser.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
+const jwtSecret = "mynameisabcandabcdefghijklmnop";
 
 router.post(
   "/createuser",
   [
     body("email", "incorrect email").isEmail(),
     body("name", "incorrect name").isLength({ min: 5 }),
-    body("password"," Incorrect password").isLength({ min: 5 }),
+    body("password", " Incorrect password").isLength({ min: 5 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -17,14 +20,67 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
+    let password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(password, salt);
+
     try {
       await User.create({
         name: req.body.name,
         location: req.body.location,
         email: req.body.email,
-        password: req.body.password,
+        password: secPassword,
       });
       res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+
+router.post(
+  "/loginuser",
+  [
+    body("email", "incorrect email").isEmail(),
+    body("password", " Incorrect password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log(email, password);
+    try {
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res.status(400).json({
+          error: "Try logging with correct credentials",
+        });
+      }
+      const pwdCompare = await bcrypt.compare(password, userData.password);
+
+      if (!pwdCompare) {
+        return res.status(400).json({
+          error: "Try logging with correct credentials",
+        });
+      }
+
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+
+      return res.json({
+        success: true,
+        authToken: authToken,
+      });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
